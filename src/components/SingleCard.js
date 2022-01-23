985dc09bf6684349db7642e1be6c80bb0301960b
Vasil Krumov <vassil.krumov@gmail.com>
2022-01-23 22:12:09 +0200

    :tada: Completed Project

---
+++ src/components/SingleCard.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './SingleCard.css'
+
+const SingleCard = ({ card, handleChoice, flipped, disabled }) => {
+    const handleClick = () => {
+        if (!disabled) {
+            handleChoice(card)
+        }
+    }
+    return (
+        <div className="card">
+            <div className={flipped ? 'flipped' : ''}>
+                <img src={card.src} alt="card front" className="front" />
+                <img src="/img/card-cover.jpg" alt="card back" className="back" onClick={handleClick} />
+            </div>
+        </div>
+    )
+}
+
+export default SingleCard
